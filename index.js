16a89a9137eb654c876b7cda38617b7f81db3150
victoronyonkaoroo <victor.oroo@student.moringaschool.com>
2023-06-15 22:18:49 +0300

    done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+var customerName = 'bob';
+const leastFavoriteCustomer = 'mary';
+
+function upperCaseCustomerName() {
+    if (customerName) {
+        customerName = customerName.toUpperCase();
+        console.log(customerName);
+        
+    } else {
+        console.log('customerName is not defined');
+        
+    }
+    
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+    
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+    
+}
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'mary';
+    
+}
